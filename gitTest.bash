45af8c597986579c8568c150b2afaba1847e95e1
AlexLadino <jair.ladino@correounivalle.edu.co>
2020-07-28 22:56:56 -0500

    subida

--- gitTest.bash
+++ gitTest.bash
@@ -7,6 +7,6 @@ cd ${0%/*} || exit 1    # Run from this directory
 # git clone https://github.com/AlexLadino/gitTest.git # funcionando
 
 git add .
-git commit -m $1
+git commit -m "subida"
 
 git push https://alexladino:caduqued2020@github.com/alexladino/gitTest.git --all
