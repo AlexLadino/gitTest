772fb227078f749b2e71d64cea96af3afc4e2b0c
AlexLadino <jair.ladino@correounivalle.edu.co>
2020-07-28 22:38:30 -0500

    includePost

---
+++ gitTest.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ${0%/*} || exit 1    # Run from this directory
+
+# Source tutorial run functions
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+# git clone https://github.com/AlexLadino/gitTest.git # funcionando
+
+git add .
+git commit -m $1
+
+git push https://alexladino:caduqued2020@github.com/alexladino/gitTest.git --all
